f6873a9c9f30dbd0491bf999b961862f74516a9e
unknown <jeferson_cambui@hotmail.com>
2022-02-21 20:30:47 -0300

    Calculadora de IMC

---
+++ imc.js
@@ -0,0 +1,12 @@
+const calcular = document.getElementById('calcular');
+
+
+function imc (){
+    const nome = document.getElementById('nome');
+    const altura = document.getElementById('altura');
+    const peso = document.getElementById('peso');
+    const resultado = document.getElementById('resultado');
+    
+}
+
+calcular.addEventListener('click', imc);
